7813f7b7cfdc038b3258c48096a4d872328124ca
Blackcatmaxy <bobthedaniel@gmail.com>
2019-05-30 17:50:40 -0700

    Updated !help and !commands to use the github wiki for command listing

--- BotCatMaxy/Misc Commands.cs
+++ BotCatMaxy/Misc Commands.cs
@@ -9,13 +9,13 @@ namespace BotCatMaxy {
     public class MiscCommands : ModuleBase<SocketCommandContext> {
         [Command("commands")]
         public async Task ListCommands() {
-            await ReplyAsync("View commands here https://docs.google.com/document/d/1uVYHX9WEe2aRy2QbzMIwHMHthxJsViqu5Ah-yFKCANc/edit?usp=sharing");
+            await ReplyAsync("View commands here https://github.com/Blackcatmaxy/Botcatmaxy/wiki");
         }
 
         [Command("help")]
         public async Task Help() {
             var embed = new EmbedBuilder();
-            embed.AddField("To see commands", "[Click here](https://docs.google.com/document/d/1uVYHX9WEe2aRy2QbzMIwHMHthxJsViqu5Ah-yFKCANc/edit?usp=sharing)", true);
+            embed.AddField("To see commands", "[Click here](https://github.com/Blackcatmaxy/Botcatmaxy/wiki)", true);
             embed.AddField("Report issues and contribute at", "[Click here for GitHub link](http://bot.blackcatmaxy.com)", true);
             await ReplyAsync(embed: embed.Build());
         }
