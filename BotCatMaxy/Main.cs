07ed2e53e1c47be56eb55e9ef4305b102e3ee507
Blackcatmaxy <bobthedaniel@gmail.com>
2019-05-30 18:18:55 -0700

    Incremented version num

--- BotCatMaxy/Main.cs
+++ BotCatMaxy/Main.cs
@@ -33,7 +33,7 @@ namespace BotCatMaxy {
             Logging logger = new Logging(_client);
             _ = TempBanChecker.Timer(_client);
 
-            await _client.SetGameAsync("version 0.6.1");
+            await _client.SetGameAsync("version 0.6.2");
 
             await handler.InstallCommandsAsync();
             logger.SetUp();
