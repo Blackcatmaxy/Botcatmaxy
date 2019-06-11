9e3c0b8f1de421099e3f95d0f6f4c1c34f60d5db
Blackcatmaxy <bobthedaniel@gmail.com>
2019-06-10 21:23:03 -0700

    Adds capability for console commands and closes #22

---
+++ BotCatMaxy/Console.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using System.Linq;
+using System.Text;
+using BotCatMaxy;
+using Discord;
+using System;
+
+
+namespace BotCatMaxy {
+    public class ConsoleReader {
+        readonly DiscordSocketClient client;
+        public ConsoleReader(DiscordSocketClient client) {
+            this.client = client;
+        }
+
+        public async Task NewInput() {
+            string input = Console.ReadLine();
+            List<string> splitInput = input.Split(' ').ToList();
+
+            if (splitInput[0].ToLower() == "messageowners") {
+                splitInput.RemoveAt(0);
+                await new LogMessage(LogSeverity.Info, "Console", "Messaging guild owners: " + splitInput).Log();
+            }
+
+            _ = NewInput();
+        }
+    }
+}
