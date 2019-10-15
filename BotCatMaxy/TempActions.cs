99f48ecd0845a394be61c5029c3e20a5fba62fc9
Daniel Bereza <blackcatmaxy@gmail.com>
2019-10-15 10:23:06 -0700

    Adds some console info to try to diagnose why tempmutes sometime fail to unmute

--- BotCatMaxy/TempActions.cs
+++ BotCatMaxy/TempActions.cs
@@ -87,7 +87,7 @@ namespace BotCatMaxy {
                                         IGuildUser gUser = guild.GetUser(tempMute.user);
                                         if (gUser?.RoleIds?.NotEmpty() ?? false && gUser.RoleIds.Contains(settings.mutedRole)) {
                                             _ = new LogMessage(LogSeverity.Error, "TempAction", 
-                                                $"User ({tempMute.user} in {guild.Name} discord) should be unmuted but is still muted. NUser null: {user == null}, Nuser is in guild {user is IGuildUser}(socket: {user is SocketGuildUser})").Log();
+                                                $"User ({tempMute.user} in {guild.Name} discord) should be unmuted but is still muted. NUser null: {user == null}, Nuser is in guild {user is IGuildUser}(socket: {user is SocketGuildUser}) with {user.MutualGuilds.Count} shared guilds").Log();
                                             await gUser.RemoveRoleAsync(guild.GetRole(settings.mutedRole));
                                         }
                                         user ??= client.GetUser(tempMute.user);
