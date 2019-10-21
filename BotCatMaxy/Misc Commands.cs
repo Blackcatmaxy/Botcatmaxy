aba3cfa4eb44bb55c57a066dab9e738baec0699f
Blackcatmaxy <bobthedaniel@gmail.com>
2019-10-20 19:34:13 -0700

    Adds a small debug command

--- BotCatMaxy/Misc Commands.cs
+++ BotCatMaxy/Misc Commands.cs
@@ -22,6 +22,19 @@ namespace BotCatMaxy {
             embed.AddField("Report issues and contribute at", "[Click here for GitHub link](http://bot.blackcatmaxy.com)", true);
             await ReplyAsync(embed: embed.Build());
         }
+        
+        [Command("checkperms")]
+        [RequireUserPermission(GuildPermission.BanMembers, Group = "Permission")]
+        [RequireOwner(Group = "Permission")]
+        public async Task CheckPerms() {
+            GuildPermissions perms = Context.Guild.CurrentUser.GuildPermissions;
+            var embed = new EmbedBuilder();
+            embed.AddField("Manage roles", perms.ManageRoles, true);
+            embed.AddField("Manage messages", perms.ManageMessages, true);
+            embed.AddField("Kick", perms.KickMembers, true);
+            embed.AddField("Ban", perms.BanMembers, true);
+            await ReplyAsync(embed: embed.Build());
+        }
 
         [RequireOwner()]
         [Command("bottest")]
