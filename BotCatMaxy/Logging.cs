a25cfab49b9861517790a27a788d3e764a495619
Daniel Bereza <bob.the.daniel@gmail.com>
2020-06-12 13:46:43 -0700

    Fixes null ref and redundant code in edit logging

--- BotCatMaxy/Logging.cs
+++ BotCatMaxy/Logging.cs
@@ -44,21 +44,22 @@ namespace BotCatMaxy {
                 if (!(channel is SocketGuildChannel)) return;
                 SocketGuild guild = (channel as SocketGuildChannel).Guild;
                 IMessage oldMessage = await cachedMessage.GetOrDownloadAsync();
-                if (oldMessage.Content == newMessage.Content || newMessage.Author.IsBot || guild == null) return;
+                if (oldMessage?.Content == newMessage.Content || newMessage.Author.IsBot || guild == null) return;
                 LogSettings settings = guild.LoadFromFile<LogSettings>();
-                if (settings == null || !settings.logEdits) return;
-                SocketTextChannel logChannel = guild.GetChannel(settings.logChannel ?? 0) as SocketTextChannel;
+                //if settings or the log channel are null, or edit logging is disabled, just stop
+                if (settings?.logChannel == null || !settings.logEdits) return;
+                SocketTextChannel logChannel = guild.GetChannel(settings.logChannel.Value) as SocketTextChannel;
                 if (logChannel == null) return;
 
                 var embed = new EmbedBuilder();
-                if (oldMessage?.Content == null || oldMessage.Content == "") {
+                if (string.IsNullOrEmpty(oldMessage?.Content)) {
                     embed.AddField($"Message was edited in #{newMessage.Channel.Name} from",
                     "`This message had no text or was null`");
                 } else {
                     embed.AddField($"Message was edited in #{newMessage.Channel.Name} from",
                     oldMessage.Content.Truncate(1020));
                 }
-                if (newMessage?.Content == null || newMessage.Content == "") {
+                if (string.IsNullOrEmpty(newMessage.Content)) {
                     embed.AddField($"Message was edited in #{newMessage.Channel.Name} to",
                     "`This message had no text or is null`");
                 } else {
@@ -67,13 +68,12 @@ namespace BotCatMaxy {
                 }
 
                 embed.AddField("Message Link", "[Click Here](" + newMessage.GetJumpUrl() + ")", false);
-
                 embed.WithFooter("ID: " + newMessage.Id)
                     .WithAuthor(newMessage.Author)
                     .WithColor(Color.Teal)
                     .WithCurrentTimestamp();
 
-                logChannel.SendMessageAsync(embed: embed.Build()).Result.GetJumpUrl();
+                await logChannel.SendMessageAsync(embed: embed.Build());
             } catch (Exception exception) {
                 await new LogMessage(LogSeverity.Error, "Logging", exception.Message, exception).Log();
             }
