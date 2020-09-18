4376a1f98fcd290203863443acf7291caf46daed
Daniel Bereza <36429439+Blackcatmaxy@users.noreply.github.com>
2020-09-18 14:53:46 -0700

    Implements "Autofilter detect similar recent messages and append new infractions" (closes #43)

--- BotCatMaxy/Components/Filter/FilterUtilities.cs
+++ BotCatMaxy/Components/Filter/FilterUtilities.cs
@@ -7,7 +7,10 @@ using Discord.Commands;
 using Discord.Rest;
 using Discord.WebSocket;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BotCatMaxy.Components.Filter
@@ -90,19 +93,12 @@ namespace BotCatMaxy.Components.Filter
         {
             string jumpLink = await DiscordLogging.LogMessage(reason, context.Message, context.Guild, color: Color.Gold, authorOveride: user);
             await user.Warn(warnSize, reason, context.Channel as SocketTextChannel, logLink: jumpLink);
-            LogSettings logSettings = context.Guild.LoadFromFile<LogSettings>(false);
-            Task<IUserMessage> warnMessage = null;
-            var pubLogChannel = await context.Guild.GetTextChannelAsync(logSettings?.pubLogChannel ?? 0);
-            if (pubLogChannel != null)
-            {
-                warnMessage = pubLogChannel.SendMessageAsync($"{user.Mention} has been given their {user.LoadInfractions().Count.Suffix()} infraction because of {reason}");
-            }
-            else
-            {
-                if (settings?.anouncementChannels?.Contains(context.Channel.Id) ?? false) //If this channel is an anouncement channel
-                    return;
-                warnMessage = context.Channel.SendMessageAsync($"{user.Mention} has been given their {user.LoadInfractions().Count.Suffix()} infraction because of {reason}");
-            }
+
+            if (settings?.anouncementChannels?.Contains(context.Channel.Id) ?? false) //If this channel is an anouncement channel
+                return;
+
+            Task<IUserMessage> warnMessage = await NotifyPunish(context, user, reason, settings);
+
             if (delete)
             {
                 try
@@ -116,6 +112,65 @@ namespace BotCatMaxy.Components.Filter
                     await warnMessage?.Result?.ModifyAsync(msg => msg.Content += ", something went wrong removing the message.");
                 }
             }
+        }
+
+        public const string notifyInfoRegex = @"<@!?(\d+)> has been given their (\d+)\w+-?(?:\d+\w+)? infraction because of (.+)";
+
+        public static async Task<Task<IUserMessage>> NotifyPunish(ICommandContext context, SocketGuildUser user, string reason, ModerationSettings settings)
+        {
+            Task<IUserMessage> warnMessage = null;
+            LogSettings logSettings = context.Guild.LoadFromFile<LogSettings>(false);
+
+            string infractionAmount = user.LoadInfractions().Count.Suffix();
+
+            var messages = (await context.Channel.GetMessagesAsync(5).FlattenAsync()).Where(msg => msg.Author.Id == context.Client.CurrentUser.Id);
+            if (messages.MatchInMessages(user.Id, out Match match, out IMessage message))
+            {
+                int oldInfraction = int.Parse(match.Groups[2].Value);
+                await (message as IUserMessage).ModifyAsync(msg => msg.Content = $"{user.Mention} has been given their {oldInfraction.Suffix()}-{infractionAmount} infraction because of {reason}");
+                //if (string.IsNullOrEmpty(match.Groups[3].Value))
+                //{//If need to go from "@person has been given their 3rd infractions because of fdsfd" to "@person has been given their 3rd-4th infractions because of fdsfd"
+                //    await (message as IUserMessage).ModifyAsync(msg => msg.Content = $"{user.Mention}");
+                //}
+                //else
+                //{
+                //    await (message as IUserMessage).ModifyAsync(msg => msg.Content = $"");
+                //}
+                return null;
+            } else
+            {
+                string toSay = $"{user.Mention} has been given their {infractionAmount} infraction because of {reason}";
+                var pubLogChannel = await context.Guild.GetTextChannelAsync(logSettings?.pubLogChannel ?? 0);
+                if (pubLogChannel != null)
+                {
+                    warnMessage = pubLogChannel.SendMessageAsync(toSay);
+                }
+                else
+                {
+
+                    warnMessage = context.Channel.SendMessageAsync(toSay);
+                }
+                return warnMessage;
+            }
+        }
+
+        public static bool MatchInMessages(this IEnumerable<IMessage> messages, ulong userID, out Match match, out IMessage successMessage)
+        {
+            match = null;
+            successMessage = null;
+            if (!messages.Any()) return false;
+            foreach (IMessage message in messages.ToArray().OrderBy(msg => msg.Timestamp))
+            {
+                var result = Regex.Match(message.Content, notifyInfoRegex);
+                if (result.Success && result.Groups[1].Value == userID.ToString())
+                {
+                    successMessage = message;
+                    match = result;
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
