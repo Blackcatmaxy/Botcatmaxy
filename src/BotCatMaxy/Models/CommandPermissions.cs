7b1f4e798c3a6104037f7da0f518e11957788314
Daniel Bereza <blackcatmaxy@gmail.com>
2021-06-16 18:18:54 -0700

    Adds wildcards to the nodes and command permission verification

--- src/BotCatMaxy/Models/CommandPermissions.cs
+++ src/BotCatMaxy/Models/CommandPermissions.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using BotCatMaxy.Data;
 using Discord;
 using MongoDB.Bson.Serialization.Attributes;
@@ -14,11 +16,44 @@ namespace BotCatMaxy.Models
         [BsonDictionaryOptions(DictionaryRepresentation.ArrayOfArrays)]
         public Dictionary<ulong, List<string>> Map { get; } = new();
 
+        /// <summary>
+        /// Check if role has permission node
+        /// </summary>
+        /// <param name="role">Role ID</param>
+        /// <param name="value">Permission node to check</param>
         public bool RoleHasValue(ulong role, string value)
         {
-            if (!Map.TryGetValue(role, out var values))
+            if (!Map.TryGetValue(role, out var values) || values == null)
                 return false;
-            return values != null && values.Contains(value);
+
+            var valueSplit = value.Split('.');
+            foreach (var roleValue in values)
+            {
+                if (roleValue.Equals(value, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+                var roleValueSplit = roleValue.Split('.').ToList();
+                
+                if (value.Length > roleValueSplit.Count && roleValueSplit[^1] != "*")
+                    continue;
+                
+                if (roleValueSplit[^1] == "*")
+                    roleValueSplit.Remove("*");
+
+                bool result = true;
+                for (int i = 0; i < roleValueSplit.Count; i++)
+                {
+                    if (!roleValueSplit[i].Equals(valueSplit[i], StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+
+                if (result)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
